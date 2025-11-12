beb53183bcdcfedb4a70e9aa95ba26361b142518
strike20023 <strike20023@gmail.com>
2025-11-12 10:31:20 +0800

    feat(train_data): 添加测试数据集处理并输出multiwoz_test.parquet
    添加从testListFile.json读取测试数据集的功能，过滤出测试集任务ID并保存为multiwoz_test.parquet文件

--- trainer/train_data/make_train_data.py
+++ trainer/train_data/make_train_data.py
@@ -4,7 +4,18 @@ import pandas as pd
 def get_multiwoz_data():
     df = pd.read_csv("trainer/train_data/multiwoz.id", header=None)
     df.columns = ["task_id"]
+    print(df)
     df.to_parquet("trainer/train_data/multiwoz.parquet", index=False)
+
+    _df = pd.read_csv("data/multiwoz/testListFile.json", header=None)
+    _df.columns = ["task_id"]
+    df = df[df["task_id"].apply(lambda x:x in _df["task_id"].tolist())]
+    print(df)
+    df.to_parquet("trainer/train_data/multiwoz_test.parquet", index=False)
     return df
 get_multiwoz_data()
+# %%
+import datasets
+parquet_file = "trainer/train_data/multiwoz.parquet"
+datasets.load_dataset("parquet", data_files=parquet_file)["train"]
 # %%
